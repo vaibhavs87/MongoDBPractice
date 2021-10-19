2e257d4068d9d32b9da237f6f39f7676ec66ad5e
Kajal D <Kajal D@DESKTOP-EQM4REA>
2021-10-19 17:56:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDBPractice
+{
+    [BsonIgnoreExtraElements]
+    class Shipwrecks
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+       
+        [BsonElement("feature_type")]
+        public string FeatureType { get; set; }
+        
+        [BsonElement("chart")]
+        public string Chart { get; set; }
+
+        [BsonElement("latdec")]
+        public double Lattitude { get; set; }
+
+        [BsonElement("londec")]
+        public double Longitude { get; set; }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LoadData();
+            Console.ReadLine();
+        }
+
+        public static void LoadData()
+        {
+            var client = new MongoClient("mongodb+srv://vaibhav:test@cluster0.ue8gu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+            var database = client.GetDatabase("sample_geospatial");
+            var collection = database.GetCollection<Shipwrecks>("shipwrecks");
+            var FilterCollection = collection.Find(r => r.FeatureType == "Wrecks - Visible").ToList();
+
+            foreach (Shipwrecks item in FilterCollection)
+            {
+                Console.WriteLine($"ID:"+ item.Id);
+                Console.WriteLine($"FeatureType:" +  item.FeatureType);
+                Console.WriteLine($"Lattitude:" + item.Lattitude);
+                Console.WriteLine($"Longitude:" +  item.Longitude);
+                Console.WriteLine("------------------------------------------------");
+            }
+        }
+    }
+}
