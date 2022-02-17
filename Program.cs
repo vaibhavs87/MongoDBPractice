62fdc309870c884cfcca2ecd9e58a9c74d1f922b
Vaibhav Mahendra Sonawane <vaibhavs87@gmail.com>
2022-02-17 23:36:03 +0530

    Added some code

--- Program.cs
+++ Program.cs
@@ -3,9 +3,30 @@ using MongoDB.Driver;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MongoDBPractice
 {
+    public class MyClass
+    {
+        public int ID { get; set; }
+        public int Year { get; set; }
+        public string X { get; set; }
+    }
+
+    public class MyClassEqualityComparer : IEqualityComparer<MyClass>
+    {
+        public bool Equals(MyClass x, MyClass y)
+        {
+            return x.Year == y.Year;
+        }
+
+        public int GetHashCode(MyClass obj)
+        {
+            return obj.ToString().ToLower().GetHashCode();
+        }
+    }
     // Class for model
     //Comments
     //Comments
@@ -36,9 +57,18 @@ namespace MongoDBPractice
     {
         static void Main(string[] args)
         {
+            var fake = new List<MyClass> {
+          new MyClass { ID = 1, Year = 2011, X = "" }
+        , new MyClass { ID = 2, Year = 2012, X = "" }
+        , new MyClass { ID = 3, Year = 2013, X = "" }
+        };
 
-            LoadData();
-            Console.ReadLine();
+            var real = new List<MyClass> {
+          new MyClass { ID = 35, Year = 2011, X = "Information" }
+        , new MyClass { ID = 77, Year = 2013, X = "Important" }
+        };
+
+            var merged = real.Union(fake, new MyClassEqualityComparer());
         }
 
         public static void LoadData()
